32e4881ab91ac00cb2eb652035130fd09f50d73f
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-21 09:57:18 +0200

    postInstall & postDeploy

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,6 +3,6 @@ set -o allexport; source .env; set +o allexport;
 
 sleep 20s;
 
-sed -i "s@location / {@location / {\n\nsub_filter 'http://'  'https://';\nsub_filter_once off;\n\n@g" /opt/elestio/nginx/conf.d/${DOMAIN}.conf
+sed -i 's@location / {@location / {\n\nsub_filter "http://"  "https://";\nsub_filter_once off;\n\n@g' /opt/elestio/nginx/conf.d/${DOMAIN}.conf
 
 docker exec elestio-nginx nginx -s reload;
