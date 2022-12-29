b958c394e09a4d570d4cceba9f7938484b7a2d6b
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-29 11:39:15 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,9 +1,9 @@
-set env vars
-set -o allexport; source .env; set +o allexport;
+# set env vars
+# set -o allexport; source .env; set +o allexport;
 
-sleep 20s;
+# sleep 20s;
 
 # sed -i 's@location / {@location / {\n\nsub_filter "http://"  "https://";\nsub_filter_once off;\n\n@g' /opt/elestio/nginx/conf.d/${CI_CD_DOMAIN}.conf
-sed -i 's@proxy_pass http://@proxy_pass https://@g' /opt/elestio/nginx/conf.d/${CI_CD_DOMAIN}.conf
+# # sed -i 's@proxy_pass http://@proxy_pass https://@g' /opt/elestio/nginx/conf.d/${CI_CD_DOMAIN}.conf
 
-docker exec elestio-nginx nginx -s reload;
+# docker exec elestio-nginx nginx -s reload;
