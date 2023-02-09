87f0a2efabe125d08769f889a9598d9db3137dd4
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-02-09 15:09:47 +0200

    fix

--- scripts/postDeploy.sh
+++
@@ -1,7 +0,0 @@
-# set env vars
-set -o allexport; source .env; set +o allexport;
-
-echo "Waiting for Friendica to be ready..."
-sleep 30s;
-
-sed -i "s@'ssl_policy' => 2,@'ssl_policy' => 1,@g" ./friendica/config/local.config.php
