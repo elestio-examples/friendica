a0205fca3e22658f666f004a0b92cf1adee2a3e5
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-13 15:08:23 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -5,7 +5,7 @@ echo "Waiting for Friendica to be ready..."
 sleep 30s;
 
 sed -i "s~DOMAIN_TO_CHANGE~${CI_CD_DOMAIN}~g" ./docker-compose.yml
-sed -i "s~172.17.0.1~${IP}~g" ./docker-compose.yml
+sed -i "s~0.0.0.0~${IP}~g" ./docker-compose.yml
 sed -i "s@'ssl_policy' => 2,@'ssl_policy' => 1,@g" ./friendica/config/local.config.php
 
 docker-compose down;
