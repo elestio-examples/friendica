a2e7eb7cf2121babe5e423ff4ee9ad94c0ba0345
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-06-13 09:53:37 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,4 +4,5 @@ set -o allexport; source .env; set +o allexport;
 echo "Waiting for Friendica to be ready..."
 sleep 30s;
 
+sed -i "s~DOMAIN_TO_CHANGE~${CI_CD_DOMAIN}~g" ./docker-compose.yml
 sed -i "s@'ssl_policy' => 2,@'ssl_policy' => 1,@g" ./friendica/config/local.config.php
